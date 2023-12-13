b9e0983f8470ffbfd8aa25885f949b238796bf00
Valda <valda.bimbirule1@gmail.com>
2023-12-13 18:11:41 +0200

    Update in the script

--- jenkins/docker.sh
+++ jenkins/docker.sh
@@ -5,6 +5,14 @@ DOCKER_IMAGE="jenkins/jenkins:lts"
 CONTAINER_NAME="jenkins"
 JENKINS_PORT="8080"
 
+#CHECK IF CONTAINER ALREADY EXISTS
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    echo "Container already exists."
+    echo "Stopping and removing existing Jenkins container..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+fi
+
 #RUN JENKINS CONTAINER
 echo "Starting Jenkins container ..."
 docker run -p $JENKINS_PORT:8080 -p 5000:5000 -d -v jenkins_home:/var/jenkins_home --name $CONTAINER_NAME $DOCKER_IMAGE
