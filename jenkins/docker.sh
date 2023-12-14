bdd734dc6b4ddda21dbc9bf993a33bede5085b8a
Valda <valda.bimbirule1@gmail.com>
2023-12-14 14:33:15 +0200

    Bash error messages

--- jenkins/docker.sh
+++ jenkins/docker.sh
@@ -9,10 +9,19 @@ JENKINS_PORT="8080"
 if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
     echo "Container already exists."
     echo "Stopping and removing existing Jenkins container..."
-    docker stop $CONTAINER_NAME
-    docker rm $CONTAINER_NAME
+    docker stop $CONTAINER_NAME || {
+        echo "Failed to stop the container."
+        exit 1
+    }
+    docker rm $CONTAINER_NAME || {
+        echo "Failed to remove the container."
+        exit 1
+    }
 fi
 
 #RUN JENKINS CONTAINER
 echo "Starting Jenkins container ..."
-docker run -p $JENKINS_PORT:8080 -p 5000:5000 -d -v jenkins_home:/var/jenkins_home --name $CONTAINER_NAME $DOCKER_IMAGE
+docker run -p $JENKINS_PORT:8080 -p 5000:5000 -d -v jenkins_home:/var/jenkins_home --name $CONTAINER_NAME $DOCKER_IMAGE || {
+    echo "Failed to start Jenkins container."
+    exit 1
+}
